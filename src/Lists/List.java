e9870fdf2d74da83056fa497e74924dd479f541b
Ninjasuperestudioso <jocamposra@est.utn.ac.cr>
2025-06-26 14:21:17 -0600

    Se crea la clase list con sus metodos

---
+++ src/Lists/List.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Lists;
+
+/**
+ *
+ * @author Joan
+ */
+public interface List <T>{
+    public boolean add(T t);
+    public boolean delete(T t);
+    public void sort();
+    public T search(Object id);
+      
+}
